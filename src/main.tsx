d1b8470cb03cb5216e64332921a72a32edc05948
Kelvin Seibt <kelvin.seibt@gmail.com>
2025-04-14 00:10:46 -0300

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import '@fontsource-variable/inter';
+import '@fontsource/jetbrains-mono/400.css';
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
